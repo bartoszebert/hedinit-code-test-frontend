39463e16f03cf3c4d04b25a0b412e8c8660a98e3
Bartosz Ebert <bartoszebert@gmail.com>
2024-04-10 16:13:57 +0200

    car details page on mobile more responsive - fonts

--- app/cars/[id]/page.tsx
+++ app/cars/[id]/page.tsx
@@ -14,8 +14,8 @@ const CarDetailsPage = async ({ params }: { params: { id: string } }) => {
   const carDetails = await getCarDetails(carSlug)!;
 
   return (
-    <div className="flex flex-col items-center p-24 prose-sm">
-      <h1>Car: {carDetails?.title}</h1>
+    <div className="flex flex-col items-center p-5 lg:p-24 prose-sm">
+      <h1 className="text-xl sm:text-3xl text-center">Car: {carDetails?.title}</h1>
       <Link className="bg-yellow-300 p-2 mt-3 p-2 px-4" href="/">
         Back to cars
       </Link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,9 @@ const RootLayout = ({
   return (
     <html lang="en">
       <body className="flex flex-col w-full justify-center">
-        <div className="flex flex-col w-full items-center w-full prose-lg text-center px-[30px]">
-          <h1 className="mt-5">Text lorem ipsum</h1>
-          <p className="mt-1 lg:max-w-screen-md">
+        <div className="flex flex-col w-full items-center w-full text-center px-[30px]">
+          <h1 className="mt-5 text-3xl md:text-4xl lg:text-5xl">Text lorem ipsum</h1>
+          <p className="mt-3 lg:max-w-screen-md lg:text-xl font-thin">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
             cupiditate autem aliquid fugiat voluptate laboriosam impedit. Quas
             quae dolores cum.
